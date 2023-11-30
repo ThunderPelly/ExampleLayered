332b6a57c972996e6306166eb77c69c43afe08bf
Peter Spielhofer <peter.spielhofer@gmx.net>
2023-11-30 22:15:36 +0100

    fix coding (umlauts)

--- src/main/kotlin/com/example/layered/model/User.kt
+++ src/main/kotlin/com/example/layered/model/User.kt
@@ -6,17 +6,17 @@ import kotlin.math.min
 @JvmInline
 value class UserName private constructor(val value: String) {
     init {
-        // eine IllegalArgumentException auslösen, wenn userName leer ist
+        // eine IllegalArgumentException auslÃ¶sen, wenn userName leer ist
         require(value.isNotBlank()) { "Der Benutzername darf nicht leer sein." }
     }
 
     companion object {
         operator fun invoke(value: String?, suffix: Int = 0): UserName {
-            // eine IllegalArgumentException auslösen, wenn userName leer ist
+            // eine IllegalArgumentException auslÃ¶sen, wenn userName leer ist
             requireNotNull(value) { "Der Benutzername darf nicht null sein." }
-            // begrenzen der Länge des Basisbenutzernamens auf maximal 5 Zeichen
+            // begrenzen der LÃ¤nge des Basisbenutzernamens auf maximal 5 Zeichen
             val truncatedValue = value.substring(0, min(value.length, 5))
-            // anhängen des Suffixes an den abgeschnittenen Wert
+            // anhÃ¤ngen des Suffixes an den abgeschnittenen Wert
             val finalValue = if (suffix > 0) "$truncatedValue$suffix" else truncatedValue
 
             return UserName(finalValue)
